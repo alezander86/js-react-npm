81a87b36a09d562ee5a4eba279a73ecfb72c93dd
oleksandr_taruraiev <oleksandr_taruraiev@epam.com>
2025-07-18 22:32:47 +0300

    Align image

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello, EDP!</h1>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello, KubeRocketCI!
+        </p>
+      </header>
+    </div>
+  );
 }
 
 export default App;
